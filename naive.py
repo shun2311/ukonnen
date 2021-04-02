34474d7712d8b74b06692ed04ff21fdea74d03cd
shun <songshunnew@hotmail.com>
2021-04-02 19:22:28 +0800

    partially working naive suffix tree construction

---
+++ naive.py
@@ -0,0 +1,70 @@
+import ukkonnen
+
+def get_index(char):
+    if char != '$':
+        return ord(char) - 96
+    else:
+        return 0
+
+def traverse(node,suffix):
+    active_node = node
+    active_len = 0
+    i = 0
+    index = get_index(suffix[0])
+
+    if active_node.edge[index]==0:
+        return (i, active_len, active_node)
+    else:
+        string = active_node.edge[index].string
+    while i<len(suffix):
+        active_len = 0
+        #traverse through current edge
+        while active_len<len(string):
+            #branch at edge
+            if string[active_len]!=suffix[i]:
+                return (i, active_len, active_node)
+            active_len += 1
+            i+=1
+        index = get_index(string[active_len-1])
+        active_node = active_node.edge[index].next
+        index = get_index(suffix[i])
+        if active_node.edge[index]==0:
+            return (i, active_len, active_node)
+
+
+def build_suffix_tree(string):
+    i = 0
+    string = string+"$"
+    tree = ukkonnen.Tree()
+    while i<len(string):
+        suffix = string[i:len(string)]
+        node = tree.root
+        traverse_tuple = traverse(node,suffix)
+        suffix_index = traverse_tuple[0]
+        active_len = traverse_tuple[1]
+        active_node = traverse_tuple[2]
+        index = get_index(suffix[0])
+
+        #branch from node
+        if active_len == 0:
+            active_node.edge[index] = ukkonnen.Edge(suffix)
+        #branch from edge
+        else:
+            text_branch = active_node.edge[index].string
+            before_branch = text_branch[0:active_len]
+            after_branch = text_branch[active_len:len(text_branch)]
+            suffix_branch = suffix[suffix_index:len(suffix)]
+            active_node.edge[index].string = before_branch
+            char_index = get_index(after_branch[0])
+            active_node.edge[index].next.edge[char_index] = ukkonnen.Edge(after_branch)
+            char_index = get_index(suffix[0])
+            active_node.edge[index].next.edge[char_index] = ukkonnen.Edge(suffix_branch)
+            #print("tree_node: "+str(tree.root.edge[index].next.edge))
+        i+=1
+    
+    return tree
+
+text = "abaa"
+tree = build_suffix_tree(text)
+print(tree.root.edge[1].next.edge[0])
+
--- ukkonnen.py
+++ ukkonnen.py
@@ -1,32 +1,15 @@
-import suffix_tree
-def traverse(node,suffix):
-    if suffix[0]!='$':
-        index = ord(suffix[0]) - 96
-    else:
-        index = 0
-    active_node = node
-    if active_node.edge[index]==0:
-        return active_node
+class Tree:
+    def __init__(self):
+        self.root = Node()
 
-def build_suffix_tree(string):
-    i = 0
-    string = string+"$"
-    tree = suffix_tree.Tree()
-    while i<len(string):
-        suffix = string[i:len(string)]
-        node = tree.root
-        active_node = traverse(node,suffix)
-        if suffix[0]!='$':
-            index = ord(suffix[0]) - 96
-        else:
-            index = 0
-        if active_node != None:
-            active_node.edge[index] = suffix_tree.Edge(suffix, suffix_tree.Node())
-        i+=1
+class Node:
+    def __init__(self):
+        self.edge = [0]*27
+        
+class Edge:
+    def __init__(self, string):
+        self.string = string
+        self.next = Node()
     
-    return tree
-
-text = "abaa"
-tree = build_suffix_tree(text)
-print(tree.root.edge[0])
-
+    def __str__(self):
+        return self.string
