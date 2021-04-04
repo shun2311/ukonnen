68dc23a7c09cfc0818c6088c81dc2ee2c7587572
shun <songshunnew@hotmail.com>
2021-04-04 20:25:42 +0800

    ukkonnen with basic rule 2

--- ukkonnen.py
+++ ukkonnen.py
@@ -3,9 +3,11 @@ class Tree:
         self.root = Node()
 
 class Node:
-    def __init__(self):
+    def __init__(self, is_leaf = True, suffix_id=None):
         self.edge = [0]*27
-        
+        self.is_leaf = is_leaf
+        self.suffix_id = suffix_id
+
 class Edge:
     def __init__(self, string):
         self.string = string
@@ -13,3 +15,33 @@ class Edge:
     
     def __str__(self):
         return self.string
+
+def rule_1():
+    return True
+
+def get_index(char):
+    if char != '$':
+        return ord(char) - 96
+    else:
+        return 0    
+def ukkonnen(string):
+    i = 0
+    j = 0
+    #phase 0
+    tree = Tree()
+    active_node = tree.root
+    string = string +'$'
+
+    while i < len(string):
+        while j<=i:
+            index = get_index(string[j])
+            #Rule 2
+            if active_node.edge[index]==0:
+                active_node.edge[index] = Edge(string[j:i+1])
+            j+=1
+        j = 0
+        i += 1
+    return tree
+
+tree  = ukkonnen("abc")
+print(tree.root.edge[])
