b30af719d96dbc5a41ac5d06924df3402633677f
shun <songshunnew@hotmail.com>
2021-04-04 21:21:12 +0800

    rule 1

--- ukkonnen.py
+++ ukkonnen.py
@@ -32,20 +32,41 @@ def ukkonnen(string):
 
     while i < len(string):
         while j<=i:
+            active_node = tree.root
             index = get_index(string[j])
             #Rule 2
             if active_node.edge[index]==0:
                 active_node.edge[index] = Edge(j, i)
             #traverse through tree
-            #else:
-                #complete_traverse = False
-                #while not complete_traverse:
+            else:
+                active_len = 0
+                start = active_node.edge[index].start
+                end = active_node.edge[index].end
+                while active_len < end - start:
+                    #Rule 2 branch from edge
+                    if string[start+active_len]!=string[j+active_len]:
+                        old_end = end
+                        branch_start = start
+                        #update end 
+                        active_node.edge[index].end = start + active_len
+                        active_node.edge[index].is_leaf = False
+                        index = get_index(string[start + active_len + 1])
+                        #extend old string
+                        active_node.edge[index] = Edge(start + active_len + 1,end)
+                        #branch for new suffix
+                        index = get_index(string[j + active_len])
+                        active_node.edge[index] = Edge(j + active_len + 1,i)
+                    active_len+=1
+                #Rule 1 
+                if active_node.edge[index].next.is_leaf:
+                    active_node.edge[index].end = i
 
             j+=1
         j = 0
         i += 1
     return tree
 
-tree  = ukkonnen("abc")
+tree  = ukkonnen("abaa")
+#print(tree.root.edge)
 print(tree.root.edge[2].start)
 print(tree.root.edge[2].end)
