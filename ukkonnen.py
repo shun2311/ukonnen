96f89a6e544e9a0b435adfed1804bcec17f59985
shun <songshunnew@hotmail.com>
2021-04-06 14:43:39 +0800

    refactored ukkonnen into a class

--- ukkonnen.py
+++ ukkonnen.py
@@ -18,77 +18,82 @@ def get_index(char):
     if char != '$':
         return ord(char) - 96
     else:
-        return 0   
+        return 0 
 
-def traverse(active_node,index, string, j, i): 
-    active_len = 0
-    start = active_node.edge[index].start
-    end = active_node.edge[index].end
-    count = 0
-    
-    while j+count<=i:
-        #rule 2
-        if string[j+count]!=string[start+active_len]:
-            old_end = end
-            #set new end for old branch
-            active_node.edge[index].next.is_leaf = False
-            active_node.edge[index].end = start+active_len-1
-            #set new active node to branch out
-            active_node = active_node.edge[index].next
-            index = get_index(string[start+active_len])
-            active_node.edge[index] = Edge(start+active_len, end)
-            index = get_index(string[j+count])
-            active_node.edge[index] = Edge(j+count, i)
-            return
+class SuffixTree:
+    def __init__(self, text):
+        self.tree = Tree()
+        self.text = text 
         
-        #reach end of edge, either:
-        #1) extend leaf
-        #2) move to next node
-        if start+active_len == end and j+count<i:
-            #Rule 1: reach a leaf, extend the leaf
-            if active_node.edge[index].next.is_leaf and i-(j+count)==1:
-                active_node.edge[index].end = i
+        self.ukkonnen()
+
+    def traverse(self, active_node, index, j, i): 
+        active_len = 0
+        start = active_node.edge[index].start
+        end = active_node.edge[index].end
+        count = 0
+        
+        while j+count<=i:
+            #rule 2
+            if self.text[j+count]!=self.text[start+active_len]:
+                old_end = end
+                #set new end for old branch
+                active_node.edge[index].next.is_leaf = False
+                active_node.edge[index].end = start+active_len-1
+                #set new active node to branch out
+                active_node = active_node.edge[index].next
+                index = get_index(self.text[start+active_len])
+                active_node.edge[index] = Edge(start+active_len, end)
+                index = get_index(self.text[j+count])
+                active_node.edge[index] = Edge(j+count, i)
                 return
-            active_node = active_node.edge[index].next
-            index = get_index(string[j+count+1])
-            #Rule 2: branch does not exist, extend branch from node
-            if active_node.edge[index] == 0:
-                active_node.edge[index] = Edge(j+count+1,i)
-                return
-            #else continue to traverse through tree
-            active_len=0
+            
+            #reach end of edge, either:
+            #1) extend leaf
+            #2) move to next node
+            if start+active_len == end and j+count<i:
+                #Rule 1: reach a leaf, extend the leaf
+                if active_node.edge[index].next.is_leaf and i-(j+count)==1:
+                    active_node.edge[index].end = i
+                    return
+                active_node = active_node.edge[index].next
+                index = get_index(string[j+count+1])
+                #Rule 2: branch does not exist, extend branch from node
+                if active_node.edge[index] == 0:
+                    active_node.edge[index] = Edge(j+count+1,i)
+                    return
+                #else continue to traverse through tree
+                active_len=0
+                count+=1
+                start = active_node.edge[index].start
+                end = active_node.edge[index].end
+                continue
+            active_len+=1
             count+=1
-            start = active_node.edge[index].start
-            end = active_node.edge[index].end
-            continue
-        active_len+=1
-        count+=1
 
-def ukkonnen(string):
-    i = 0
-    #phase 0
-    tree = Tree()
-    active_node = tree.root
-    string = string +'$'
+    def ukkonnen(self):
+        i = 0
+        #phase 0
+        active_node = self.tree.root
+        self.text = self.text +'$'
 
-    while i < len(string):
-        j = 0
-        while j<=i:
-            active_node = tree.root
-            index = get_index(string[j])
-            #Rule 2
-            if active_node.edge[index]==0:
-                active_node.edge[index] = Edge(j, i)
-            else:
-                traverse(active_node,index, string, j, i)    
-            j+=1
-        i += 1
-    return tree
+        while i < len(self.text):
+            j = 0
+            while j<=i:
+                active_node = self.tree.root
+                index = get_index(self.text[j])
+                #Rule 2
+                if active_node.edge[index]==0:
+                    active_node.edge[index] = Edge(j, i)
+                else:
+                    self.traverse(active_node,index, j, i)    
+                j+=1
+            i += 1
 
 string = 'abac'
-tree  = ukkonnen(string)
+suffix_tree  = SuffixTree(string)
 string = 'abaa$'
 #print(tree.root.edge)
-print(tree.root.edge[1].next.edge[2].start)
-print(tree.root.edge[1].next.edge[2].end)
+print(suffix_tree.tree.root.edge[0].start)
+print(suffix_tree.tree.root.edge[0].end)
 #print(string[tree.root.edge[1].next.edge[1].start:tree.root.edge[1].next.edge[1].end+1])
